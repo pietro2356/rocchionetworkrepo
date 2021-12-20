a2bb00bc6c3aa3599f80e5544f4c2a8f61fed47b
pietro2356 <pietro.rocchio@gmail.com>
2021-12-20 10:32:34 +0100

    Aggiunti bottoni per cambio sfondo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'rocchionetwork.com';
+
+  changeColor(color:string){
+    switch(color){
+      case 'Purple':
+        alert('Purple');
+        break;
+      case 'Peach':
+        alert('Peach');
+        break;
+      case 'Sunset':
+        alert('Sunset');
+        break;
+      case 'Night':
+        alert('Night');
+        break;
+      case 'Synthwave':
+        alert('Synthwave');
+        break;
+      case 'Grass':
+        alert('Grass');
+        break;
+      case 'Ocean':
+        alert('Ocean');
+        break;
+      default: 
+        alert('none');
+    }
+  }
 }
